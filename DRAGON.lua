56a080c1f03686f2571f47058d612b366652e561
WaTaNtEaM <85058247+WaTaNtEaM@users.noreply.github.com>
2021-05-30 14:31:25 +0300

    Add files via upload

--- DRAGON.lua
+++ DRAGON.lua
@@ -174,7 +174,7 @@ io.popen("mkdir File_Bot")
 io.popen("cd File_Bot && rm -rf commands.lua.1") 
 io.popen("cd File_Bot && rm -rf commands.lua.2") 
 io.popen("cd File_Bot && rm -rf commands.lua.3") 
-io.popen("cd File_Bot && wget https://raw.githubusercontent.com/SRC-DRAGON/Files_Dragon/main/File_Bot/commands.lua") 
+io.popen("cd File_Bot && wget https://raw.githubusercontent.com/WaTaNtEaM/Files_Watan/main/File_Bot/commands.lua") 
 t = "\27[35m".."\nAll Files Started : \n____________________\n"..'\27[m'
 i = 0
 for v in io.popen('ls File_Bot'):lines() do
@@ -1084,7 +1084,7 @@ end
 
 if text == 'تحديث السورس ℘' and DevSoFi(msg) then 
 os.execute('rm -rf DRAGON.lua')
-os.execute('wget https://raw.githubusercontent.com/SRC-DRAGON/DRAGON/main/DRAGON.lua')
+os.execute('wget https://raw.githubusercontent.com/WaTaNtEaM/WaTaN/main/DRAGON.lua')
 send(msg.chat_id_, msg.id_,' ℘︙ تم تحديث السورس \n ℘︙ لديك اخر اصدار لسورس دراكون\n ℘︙ الاصدار » { v 1.6 }')
 dofile('DRAGON.lua')  
 end
@@ -2363,7 +2363,7 @@ end
 return false
 end
 os.execute('rm -rf DRAGON.lua')
-os.execute('wget https://raw.githubusercontent.com/SRC-DRAGON/DRAGON/main/DRAGON.lua')
+os.execute('wget https://raw.githubusercontent.com/WaTaNtEaM/WaTaN/main/DRAGON.lua')
 send(msg.chat_id_, msg.id_,' ℘︙ تم تحديث السورس \n ℘︙ لديك اخر اصدار لسورس دراكون\n ℘︙ الاصدار » { v 1.6 }')
 dofile('DRAGON.lua')  
 end
@@ -3913,7 +3913,7 @@ send(msg.chat_id_, msg.id_,t)
 end
 if text == "متجر الملفات" or text == 'المتجر' then
 if DevSoFi(msg) then
-local Get_Files, res = https.request("https://raw.githubusercontent.com/SRC-DRAGON/Files_Dragon/main/getfile.json")
+local Get_Files, res = https.request("https://raw.githubusercontent.com/WaTaNtEaM/Files_Watan/main/getfile.json")
 if res == 200 then
 local Get_info, res = pcall(JSON.decode,Get_Files);
 vardump(res.plugins_)
@@ -3951,7 +3951,7 @@ t = " ℘︙ الملف » "..file.."\n ℘︙ تم تعطيل ملف \n"
 else
 t = " ℘︙ بالتاكيد تم تعطيل ملف → "..file.."\n"
 end
-local json_file, res = https.request("https://raw.githubusercontent.com/SRC-DRAGON/Files_Dragon/main/File_Bot/"..file)
+local json_file, res = https.request("https://raw.githubusercontent.com/WaTaNtEaM/Files_Watan/main/File_Bot/"..file)
 if res == 200 then
 os.execute("rm -fr File_Bot/"..file)
 send(msg.chat_id_, msg.id_,t) 
@@ -3971,7 +3971,7 @@ t = " ℘︙ بالتاكيد تم تفعيل ملف → "..file.." \n"
 else
 t = " ℘︙ الملف » "..file.."\n ℘︙ تم تفعيل ملف \n"
 end
-local json_file, res = https.request("https://raw.githubusercontent.com/SRC-DRAGON/Files_Dragon/main/File_Bot/"..file)
+local json_file, res = https.request("https://raw.githubusercontent.com/WaTaNtEaM/Files_Wtan/main/File_Bot/"..file)
 if res == 200 then
 local chek = io.open("File_Bot/"..file,'w+')
 chek:write(json_file)
